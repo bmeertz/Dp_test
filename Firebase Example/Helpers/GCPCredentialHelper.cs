a282860a1741e32dab0c875a57267a86c49c0ad0
bmeertz <36151461+bmeertz@users.noreply.github.com>
2020-10-06 04:16:07 +0200

    Add files via upload

---
+++ Firebase Example/Helpers/GCPCredentialHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace GCP_Auth_Example.Helpers
+{
+    using GCP_Auth_Example.Models;
+    using Microsoft.Azure.KeyVault;
+    using Microsoft.Extensions.Configuration;
+    using Newtonsoft.Json;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    public class GCPCredentialHelper
+    {
+        public async static Task<string> GetGCPCredentialJson(IConfiguration _configuration, KeyVaultClient _vaultClient, string KeyvaultDNS)
+        {
+            var credential = new GCPCredentials();
+            _configuration.GetSection("GCP").Bind(credential);
+            credential.PrivateKeyId = (await _vaultClient.GetSecretAsync(KeyvaultDNS, _configuration["KeyVault:FirebasePrivateKeyIdKey"])).Value;
+            credential.PrivateKey = Regex.Unescape((await _vaultClient.GetSecretAsync(KeyvaultDNS, _configuration["KeyVault:FirebasePrivateKey"])).Value);
+            credential.ClientId = _configuration["KeyVault:FirebaseClientIdKey"];
+            credential.ClientEmail = _configuration["KeyVault:FirebaseClientEmailKey"];
+            credential.ProjectId = _configuration["KeyVault:FirebaseProjectIdKey"];
+            credential.ClientCertURL = _configuration["KeyVault:FirebaseClientCertURLKey"];
+            return JsonConvert.SerializeObject(credential);
+        }
+    }
+}
---
+++ Firebase Example/Startup.cs
@@ -0,0 +1,68 @@
+namespace GCP_Auth_Example
+{
+    using GCP_Auth_Example.Helpers;
+    using GCP_Auth_Example.Services;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Azure.KeyVault;
+    using Microsoft.Azure.Services.AppAuthentication;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            var config = new ConfigurationBuilder();
+            var builder = config
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            var keyvaultBaseURl = Configuration["KeyVaultBaseURL"];
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(
+                new KeyVaultClient.AuthenticationCallback(
+                    azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            services.AddScoped<FirebaseClient>(c =>
+            {
+                var gcpCreds = GCPCredentialHelper.GetGCPCredentialJson(Configuration, keyVaultClient, keyvaultBaseURl).GetAwaiter().GetResult();
+
+                var projectId = Configuration["GCP:ProjectId"];
+                var fireBaseClient = new FirebaseClient(gcpCreds, projectId);
+                return fireBaseClient;
+            });
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
